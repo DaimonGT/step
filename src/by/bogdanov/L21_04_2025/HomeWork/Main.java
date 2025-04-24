3d91989d32e96c7e40dee088d5545c9b32db6e8b
DaimonGT <phenomenon13@mail.ru>
2025-04-24 17:49:44 +0300

    add home Work

--- src/by/bogdanov/L21_04_2025/HomeWork/Main.java
+++ src/by/bogdanov/L21_04_2025/HomeWork/Main.java
@@ -3,6 +3,7 @@ package by.bogdanov.L21_04_2025.HomeWork;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,7 +15,7 @@ public class Main {
                 new GroceryItem("Яблоко", Category.FRUIT, 2.4, false),
                 new GroceryItem("Груша", Category.FRUIT, 3.2, false),
                 new GroceryItem("Огурцы", Category.VEGETABLE, 4.2, false),
-                new GroceryItem("Капуста", Category.VEGETABLE, 3, false),
+                new GroceryItem("Капуста", Category.VEGETABLE, 0.9, false),
                 new GroceryItem("Сок", Category.BEVERAGE, 4.97, false),
                 new GroceryItem("Молоко", Category.DAIRY, 4.97, false)
         );
@@ -38,9 +39,33 @@ public class Main {
         System.out.println(collect);
 
         // Топ‑3 самых дорогих
-        groceryItems.stream()
+        List<GroceryItem> top3 = groceryItems.stream()
                 .sorted(Comparator.comparing(GroceryItem::getPrice).reversed())
                 .limit(3)
-                .forEach(System.out::println);
+                .collect(Collectors.toList());
+        System.out.println(top3);
+
+        // Печать со скидкой
+        groceryItems.stream()
+                .forEach(groceryItem -> System.out.printf("%s: %.1f Br\n", groceryItem.getName(), groceryItem.getPrice() * 0.9));
+
+        //Есть ли товар дешевле 1 Br?
+        boolean less1Br = groceryItems.stream()
+                .anyMatch(groceryItem -> groceryItem.getPrice() < 1.0);
+        System.out.println(less1Br);
+
+        //Все ли товары дороже 0
+        boolean allMatch = groceryItems.stream()
+                .allMatch(groceryItem -> groceryItem.getPrice() > 0);
+        System.out.println(allMatch);
+
+        // Первая овощная позиция
+        Optional<GroceryItem> any = groceryItems.stream()
+                .filter(groceryItems2 -> groceryItems2.getCategory().equals(Category.VEGETABLE))
+                .findAny();
+        System.out.println(any);
+
+        //Получите List<Category> всех уникальных категорий товаров.
+        
     }
 }
